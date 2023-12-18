7c316656d8c5e0ccf2f86de7d097be6c6473f423
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-18 21:28:41 +0300

    Save

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -1,20 +1,14 @@
 ﻿// Найти максимум из 9 чисел
-int Max(int arg1, int arg2, int arg3);//эта функция будет находить максимум из этих чисел, затем сравним три числа
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+int Max(int arg1, int arg2, int arg3)//эта функция будет находить максимум из этих чисел, затем сравним три числа
 {
 	int result = arg1; //задаем первый аргумент - максимум
 	if (arg2 > result) result = arg2;
 	if (arg3 > result) result = arg3;
 	return result;
 }
-int a1 = 25;
-int b1 = 17;
-int c1 = 94;
-int a2 = 42;
-int b2 = 19;
-int c2 = 81;
-int a3 = 40;
-int b3 = 69;
-int c3 = 5;
 
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
 
 Console.WriteLine(max);
